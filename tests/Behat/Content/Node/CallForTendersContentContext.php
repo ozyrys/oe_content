52a59f6d1f9d1ce2a95fd720f8725a2a7d9976bf
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2021-04-16 15:25:05 +0300

    EWPP-963: Fix subject field value for tests.

--- tests/Behat/Content/Node/CallForTendersContentContext.php
+++ tests/Behat/Content/Node/CallForTendersContentContext.php
@@ -88,7 +88,7 @@ class CallForTendersContentContext extends RawDrupalContext {
 
     // Set default fields.
     $scope->addFields([
-      'oe_subject' => 'http://data.europa.eu/uxp/10',
+      'oe_subject' => 'http://data.europa.eu/uxp/1010',
       'oe_content_content_owner' => 'http://publications.europa.eu/resource/authority/corporate-body/AGRI',
     ]);
   }
